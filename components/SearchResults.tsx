76f170dd14515f5df4002ab15f24e60493664d5c
Murillo Mesquita <mu.pracucio@gmail.com>
2021-08-10 19:00:41 -0300

    Virtualizacao

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from 'react-virtualized'
+
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -12,10 +14,30 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, onAddToWishList, totalPrice }: SearchResultsProps) {
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem 
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    )
+  }
+  
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
+
+      <List 
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
+      {/* {results.map(product => {
         return (
           <ProductItem 
             key={product.id} 
@@ -23,7 +45,7 @@ export function SearchResults({ results, onAddToWishList, totalPrice }: SearchRe
             onAddToWishList={onAddToWishList}
           />
         )
-      })}
+      })} */}
     </div>
   )
 }
